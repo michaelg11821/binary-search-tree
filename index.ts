b880998163c58040e322e392d3b45ef245137466
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-12-24 01:43:49 -0800

    partially finish brute force delete

--- index.ts
+++ index.ts
@@ -2,8 +2,15 @@ import { Tree } from "./tree";
 
 const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
+console.log("start");
 bst.prettyPrint();
 
+console.log("inserting 2...");
 bst.insert(2);
+
+bst.prettyPrint();
+
+console.log("deleting 2");
+console.log(bst.deleteItem(2));
 
 bst.prettyPrint();
--- tree.ts
+++ tree.ts
@@ -1,27 +1,27 @@
 import { TreeNode } from "./tree-node";
 import { prepareTreeArray } from "./utils";
 
-function buildTree(arr: any[], start: number, end: number) {
-  if (start > end) return null;
-
-  const mid = Math.floor((start + end) / 2);
-  const root = arr[mid] ? new TreeNode(arr[mid], null, null) : null;
-
-  if (!root) return null;
-
-  root.left = buildTree(arr, start, mid - 1);
-  root.right = buildTree(arr, mid + 1, end);
-
-  return root;
-}
-
 export class Tree {
   root;
 
   constructor(arr: any[]) {
     const treeArr = prepareTreeArray(arr);
 
-    this.root = buildTree(treeArr, 0, arr.length - 1);
+    this.root = this.buildTree(treeArr, 0, arr.length - 1);
+  }
+
+  private buildTree(arr: any[], start: number, end: number) {
+    if (start > end) return null;
+
+    const mid = Math.floor((start + end) / 2);
+    const root = arr[mid] ? new TreeNode(arr[mid], null, null) : null;
+
+    if (!root) return null;
+
+    root.left = this.buildTree(arr, start, mid - 1);
+    root.right = this.buildTree(arr, mid + 1, end);
+
+    return root;
   }
 
   prettyPrint(node: TreeNode | null = this.root, prefix = "", isLeft = true) {
@@ -45,20 +45,117 @@ export class Tree {
   }
 
   insert(value: any, currentNode: TreeNode | null = this.root) {
-    if (currentNode === null) return;
+    if (currentNode === null) return new TreeNode(value, null, null);
 
     if (value < currentNode.data) {
-      if (currentNode.left === null) {
-        currentNode.left = new TreeNode(value, null, null);
+      currentNode.left = this.insert(value, currentNode.left);
+    } else {
+      currentNode.right = this.insert(value, currentNode.right);
+    }
+
+    return currentNode;
+  }
+
+  private findNodeBefore(value: any) {
+    let currentNode = this.root;
+
+    while (currentNode !== null) {
+      if (currentNode.left && currentNode.left.data === value) {
+        return currentNode;
+      } else if (currentNode.right && currentNode.right.data === value) {
+        return currentNode;
       }
 
-      this.insert(value, currentNode.left);
-    } else if (value > currentNode.data) {
-      if (currentNode.right === null) {
-        currentNode.right = new TreeNode(value, null, null);
+      if (value < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
       }
+    }
 
-      this.insert(value, currentNode.right);
+    return null;
+  }
+
+  find(value: any) {
+    let currentNode = this.root;
+
+    while (currentNode !== null) {
+      if (currentNode.data === value) return currentNode;
+
+      if (value < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return null;
+  }
+
+  private deleteLeaf(prevNode: TreeNode | null, currentNode: TreeNode | null) {
+    if (prevNode === null) return;
+
+    if (prevNode.left === currentNode) {
+      prevNode.left = null;
+    } else if (prevNode.right === currentNode) {
+      prevNode.right = null;
+    }
+  }
+
+  private deleteNodeWithOneChild(
+    prevNode: TreeNode | null,
+    currentNode: TreeNode | null
+  ) {
+    if (currentNode === null) return;
+
+    const childNode = currentNode.left ?? currentNode.right;
+
+    if (prevNode === null) return;
+
+    if (prevNode.left === currentNode) {
+      prevNode.left = childNode;
+    } else if (prevNode.right === currentNode) {
+      prevNode.right = childNode;
+    }
+  }
+
+  // private deleteNodeWithTwoChildren(
+  //   prevNode: TreeNode | null,
+  //   currentNode: TreeNode | null
+  // ) {
+  //   if (currentNode === null) return;
+
+  //   if (currentNode.right === null) return;
+
+  //   let nextBiggestNode: TreeNode | null = currentNode.right;
+
+  //   while (nextBiggestNode.left !== null) {
+  //     nextBiggestNode = nextBiggestNode.left;
+  //   }
+
+  //   if (prevNode === null) return;
+
+  //   if (prevNode.left === currentNode) {
+  //     prevNode.left = nextBiggestNode;
+  //   } else if (prevNode.right === currentNode) {
+  //     prevNode.right = nextBiggestNode;
+  //   }
+  // }
+
+  deleteItem(value: any) {
+    const prevNode = this.findNodeBefore(value);
+    const nodeToDelete = this.find(value);
+
+    if (nodeToDelete === null) return;
+
+    if (nodeToDelete.left === null && nodeToDelete.right === null) {
+      this.deleteLeaf(prevNode, nodeToDelete);
+    }
+
+    if (nodeToDelete.left && nodeToDelete.right === null) {
+      this.deleteNodeWithOneChild(prevNode, nodeToDelete);
+    } else if (nodeToDelete.right && nodeToDelete.left === null) {
+      this.deleteNodeWithOneChild(prevNode, nodeToDelete);
     }
   }
 }
