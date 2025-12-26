4abb3fa8fdf19d0734ebfcc0f064493528764477
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-12-26 00:06:49 -0800

    create printing cb for tree traversals

--- utils.ts
+++ utils.ts
@@ -1,5 +1,11 @@
+import type { TreeNode } from "./tree-node";
+
 export function prepareTreeArray(arr: any[]) {
   const arrWithNoDupes = [...new Set(arr)];
 
   return Array.from(arrWithNoDupes).sort((a, b) => a - b);
+}
+
+export function printDataInNode(node: TreeNode) {
+  console.log(node.data);
 }
